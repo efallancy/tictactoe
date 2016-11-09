80877cd8f66285019a76018f0b08b2bc139ed663
emmafallancy <emmanuel.fallancy@gmail.com>
2016-11-10 02:42:59 +1100

    Added scoreboard

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 console.log( "Tic tac toe" );
 
-const player1 = "Player1";
-const player2 = "Player2";
+// $( "#board" ).parallax();
+
+const user = "User";
+const opponent = "Opponent";
 
 var currentPlayer = "";
-var player1Score = 0;
-var player2Score = 0;
+var winner = "";
+var userScore = 0;
+var opponentScore = 0;
 var hasWinner = false;
+var isDraw = false;
 
+// TODO: create functionality to choose character
+// TODO: create arrays of winning image display and text display (consider object)
+// TODO: if time allows, create another AI
+
+// ------------- Response ---------------
 // Set the click response
 var setClickResponse = function ( event, currentElement ) {
 
-  var currentId = $( this ).attr( "id" );
-  var isclick = $( this ).attr( "isclick" );
-
   setPlayerClickResponse( event, currentElement );
 
-  console.log( "This was being clicked", currentElement );
 };
 
 // Set the player Response
@@ -30,284 +35,243 @@ var setPlayerClickResponse = function ( event, currentElement ) {
     return;
   }
 
-  if ( currentPlayer === player1 ) {
+  playerAction( $element );
+  computerAction();
 
-    // Set to notify already clicked
-    $element.attr( "isclick", "true" );
+};
 
-    $( currentElement ).css( {
-      backgroundImage: "url( images/patrick.jpg )",
-      backgroundSize: "cover"
-    } );
+var playerAction = function ( currentElement ) {
+  var $element = $( currentElement );
 
-    // Set to notify who clicked it
-    $element.attr( "player", player1 );
+  // Set to notify already clicked
+  $element.attr( "isclick", "true" );
 
-    computerAction();
+  $element.css( {
+    backgroundImage: "url( images/patrick.jpg )",
+    backgroundSize: "cover"
+  } );
+
+  // Set to notify who clicked it
+  $element.attr( "player", user );
+
+  showWinner();
+
+};
+
+var computerAction = function () {
+  if ( hasWinner ) { return }
+  var boxes = $( ".box" );
+  var potentialBoxes = [];
+
+  for ( var i = 0; i < boxes.length; i++ ) {
+    if ( boxes.eq( i ).attr( "isclick" ) === "false" ) {
+      potentialBoxes.push( boxes.eq( i ) );
+    }
   }
-  else {
+
+  if ( potentialBoxes.length > 0 ) {
+    var luckyNumber = Math.floor( Math.random() * potentialBoxes.length );
+
+    var $computerBox = $( potentialBoxes[ luckyNumber ] );
 
     // Set to notify already clicked
-    $element.attr( "isclick", "true" );
+    $computerBox.attr( "isclick", "true" );
 
-    $element.css( {
+    $computerBox.css( {
       backgroundImage: "url( images/spongebob.png )",
       backgroundSize: "cover"
     } );
 
     // Set to notify who clicked it
-    $element.attr( "player", player2 );
+    $computerBox.attr( "player", opponent );
 
+    currentPlayer = user;
+
+    showWinner();
   }
-
-  // Evaluate the winner/result
-  console.log( evaluateWinner() );
-  var msg = evaluateWinner();
-
-  if ( evaluateWinner().length > 0 ) {
-    $( ".box" ).unbind();
+  else {
+    isDraw = true;
+    showDraw();
   }
 
 };
 
-// Create the element for the image
-var createElementImage = function ( event, source, description, title ) {
-  var img = document.createElement( "img" );
-
-  img.setAttribute( "src", source );
-  img.setAttribute( "alt", description );
-  img.setAttribute( "title", title );
-  img.setAttribute( "width", "100px" );
-  img.setAttribute( "height", "100px" );
-
-  return img;
-};
+// ------------- Triggers ---------------
 
 var init = function () {
   if ( currentPlayer === "" ) {
-    currentPlayer = player1;
+    currentPlayer = user;
   }
-  else if ( currentPlayer === player1 ) {
-    currentPlayer = player2;
+  else if ( currentPlayer === user ) {
+    currentPlayer = opponent;
   }
   else {
-    currentPlayer = player1;
+    currentPlayer = user;
   }
 
   setClickResponse( event, this );
 
 };
 
+// ------------- Evaluation ---------------
+
+var checkCombination = function () {
+  var $boxes = $( ".box" );
+
+  var userCombination = ( $boxes.eq( 0 ).attr( "player" ) === user
+                             && $boxes.eq( 1 ).attr( "player" ) === user
+                             && $boxes.eq( 2 ).attr( "player" ) === user
+                           )
+                           || ( $boxes.eq( 3 ).attr( "player" ) === user
+                                && $boxes.eq( 4 ).attr( "player" ) === user
+                                && $boxes.eq( 5 ).attr( "player" ) === user
+                              )
+                           || ( $boxes.eq( 6 ).attr( "player" ) === user
+                                && $boxes.eq( 7 ).attr( "player" ) === user
+                                && $boxes.eq( 8 ).attr( "player" ) === user
+                              )
+                           || ( $boxes.eq( 0 ).attr( "player" ) === user
+                                && $boxes.eq( 3 ).attr( "player" ) === user
+                                && $boxes.eq( 6 ).attr( "player" ) === user
+                              )
+                           || ( $boxes.eq( 1 ).attr( "player" ) === user
+                                && $boxes.eq( 4 ).attr( "player" ) === user
+                                && $boxes.eq( 7 ).attr( "player" ) === user
+                              )
+                           || ( $boxes.eq( 2 ).attr( "player" ) === user
+                                && $boxes.eq( 5 ).attr( "player" ) === user
+                                && $boxes.eq( 8 ).attr( "player" ) === user
+                              )
+                           || ( $boxes.eq( 2 ).attr( "player" ) === user
+                                && $boxes.eq( 4 ).attr( "player" ) === user
+                                && $boxes.eq( 6 ).attr( "player" ) === user
+                              )
+                           || ( $boxes.eq( 0 ).attr( "player" ) === user
+                                && $boxes.eq( 4 ).attr( "player" ) === user
+                                && $boxes.eq( 8 ).attr( "player" ) === user
+                              );
+
+  var opponentCombination = ( $boxes.eq( 0 ).attr( "player" ) === opponent
+                                && $boxes.eq( 1 ).attr( "player" ) === opponent
+                                && $boxes.eq( 2 ).attr( "player" ) === opponent
+                              )
+                              || ( $boxes.eq( 3 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 4 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 5 ).attr( "player" ) === opponent
+                                 )
+                              || ( $boxes.eq( 6 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 7 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 8 ).attr( "player" ) === opponent
+                                 )
+                              || ( $boxes.eq( 0 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 3 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 6 ).attr( "player" ) === opponent
+                                 )
+                              || ( $boxes.eq( 1 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 4 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 7 ).attr( "player" ) === opponent
+                                 )
+                              || ( $boxes.eq( 2 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 5 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 8 ).attr( "player" ) === opponent
+                                 )
+                              || ( $boxes.eq( 2 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 4 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 6 ).attr( "player" ) === opponent
+                                 )
+                              || ( $boxes.eq( 0 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 4 ).attr( "player" ) === opponent
+                                   && $boxes.eq( 8 ).attr( "player" ) === opponent
+                                 );
+
+  if ( userCombination ) {
+    hasWinner = true;
+    userScore++;
+    winner = user;
+    return winner;
+  }
+  else if ( opponentCombination ) {
+    hasWinner = true;
+    opponentScore++;
+    winner = opponent;
+    return winner;
+  }
+
+};
+
 // Check for winner
 var evaluateWinner = function () {
-  return checkComb1()
-         || checkComb2()
-         || checkComb3()
-         || checkComb4()
-         || checkComb5()
-         || checkComb6()
-         || checkComb7()
-         || checkComb8();
+  var checkcomb = checkCombination();
+  return checkcomb;
 };
 
-var checkComb1 = function () {
-  var $boxes = $( ".box" );
+var showWinner = function () {
+  // Evaluate the winner/result
+  var msg = evaluateWinner();
+  var imgDisplay = "";
 
-  var player1Combination = $boxes.eq( 0 ).attr( "player" ) === player1
-                           && $boxes.eq( 1 ).attr( "player" ) === player1
-                           && $boxes.eq( 2 ).attr( "player" ) === player1;
+  if ( hasWinner ) {
+    $( ".box" ).off();
 
-  var player2Combination = $boxes.eq( 0 ).attr( "player" ) === player2
-                           && $boxes.eq( 1 ).attr( "player" ) === player2
-                           && $boxes.eq( 2 ).attr( "player" ) === player2;
+    if ( msg === user ) {
+      imgDisplay = "images/patrick_happy.gif";
+    }
+    else if ( msg === opponent ) {
+      imgDisplay = "images/spongebob_win.gif";
+    }
 
-  if ( player1Combination ) {
-    return "Player 1 win!";
+    // Display that annoying billbord
+    displayBillboard( imgDisplay );
   }
-  else if ( player2Combination ) {
-    return "Player 2 win!";
+};
+
+var showDraw = function () {
+  imgDisplay = "images/pineapple_house.jpg";
+
+  // Display that annoying billboard
+  displayBillboard( imgDisplay );
+};
+
+var displayBillboard = function ( imgDisplay ) {
+  $( "img" ).attr( "src", imgDisplay );
+  $( "#winner_msg" ).html( winner + " win!" );
+
+  if ( isDraw ) {
+    $( "#winner_msg" ).html( "It's a draw!" );
   }
+  $( "#billboard" ).fadeIn( 500 );
 
-}
+  $( "#billboard" ).on( "click", function() {
+    $( this ).fadeOut( 500 );
+    resetGame();
+  } );
+};
 
-var checkComb2 = function () {
-  var $boxes = $( ".box" );
-
-  var player1Combination = $boxes.eq( 3 ).attr( "player" ) === player1
-                           && $boxes.eq( 4 ).attr( "player" ) === player1
-                           && $boxes.eq( 5 ).attr( "player" ) === player1;
-
-  var player2Combination = $boxes.eq( 3 ).attr( "player" ) === player2
-                           && $boxes.eq( 4 ).attr( "player" ) === player2
-                           && $boxes.eq( 5 ).attr( "player" ) === player2;
-
-  if ( player1Combination ) {
-    return "Player 1 win!";
-  }
-  else if ( player2Combination ) {
-    return "Player 2 win!";
-  }
-
-}
-
-var checkComb3 = function () {
-  var $boxes = $( ".box" );
-
-  var player1Combination = $boxes.eq( 6 ).attr( "player" ) === player1
-                           && $boxes.eq( 7 ).attr( "player" ) === player1
-                           && $boxes.eq( 8 ).attr( "player" ) === player1;
-
-  var player2Combination = $boxes.eq( 6 ).attr( "player" ) === player2
-                           && $boxes.eq( 7 ).attr( "player" ) === player2
-                           && $boxes.eq( 8 ).attr( "player" ) === player2;
-
-  if ( player1Combination ) {
-    return "Player 1 win!";
-  }
-  else if ( player2Combination ) {
-    return "Player 2 win!";
-  }
-
-}
-
-var checkComb4 = function () {
-  var $boxes = $( ".box" );
-
-  var player1Combination = $boxes.eq( 0 ).attr( "player" ) === player1
-                           && $boxes.eq( 3 ).attr( "player" ) === player1
-                           && $boxes.eq( 6 ).attr( "player" ) === player1;
-
-  var player2Combination = $boxes.eq( 0 ).attr( "player" ) === player2
-                           && $boxes.eq( 3 ).attr( "player" ) === player2
-                           && $boxes.eq( 6 ).attr( "player" ) === player2;
-
-  if ( player1Combination ) {
-    return "Player 1 win!";
-  }
-  else if ( player2Combination ) {
-    return "Player 2 win!";
-  }
-
-}
-var checkComb5 = function () {
-  var $boxes = $( ".box" );
-
-  var player1Combination = $boxes.eq( 1 ).attr( "player" ) === player1
-                           && $boxes.eq( 4 ).attr( "player" ) === player1
-                           && $boxes.eq( 7 ).attr( "player" ) === player1;
-
-  var player2Combination = $boxes.eq( 1 ).attr( "player" ) === player2
-                           && $boxes.eq( 4 ).attr( "player" ) === player2
-                           && $boxes.eq( 7 ).attr( "player" ) === player2;
-
-  if ( player1Combination ) {
-    return "Player 1 win!";
-  }
-  else if ( player2Combination ) {
-    return "Player 2 win!";
-  }
+var winnerImage = function () {
 
 };
 
-var checkComb6 = function () {
-  var $boxes = $( ".box" );
+// ------------- Reset ---------------
 
-  var player1Combination = $boxes.eq( 2 ).attr( "player" ) === player1
-                           && $boxes.eq( 5 ).attr( "player" ) === player1
-                           && $boxes.eq( 8 ).attr( "player" ) === player1;
+var resetGame = function () {
+  $( ".box" ).css( {
+    backgroundImage: "none",
+  });
+  $( ".box" ).attr( "isclick", "false" );
+  $( ".box" ).attr( "player", "" );
 
-  var player2Combination = $boxes.eq( 2 ).attr( "player" ) === player2
-                           && $boxes.eq( 5 ).attr( "player" ) === player2
-                           && $boxes.eq( 8 ).attr( "player" ) === player2;
+  // Lets show the winning counter
+  $( "#userPlayer" ).html( "Win: " + userScore );
+  $( "#opponentPlayer" ).html( "Win: " + opponentScore );
+  hasWinner = false;
+  isDraw = false;
+  winner = "";
 
-  if ( player1Combination ) {
-    return "Player 1 win!";
-  }
-  else if ( player2Combination ) {
-    return "Player 2 win!";
-  }
-
-};
-
-// Diagonal checking
-var checkComb7 = function () {
-  var $boxes = $( ".box" );
-
-  var player1Combination = $boxes.eq( 2 ).attr( "player" ) === player1
-                           && $boxes.eq( 4 ).attr( "player" ) === player1
-                           && $boxes.eq( 6 ).attr( "player" ) === player1;
-
-  var player2Combination = $boxes.eq( 2 ).attr( "player" ) === player2
-                           && $boxes.eq( 4 ).attr( "player" ) === player2
-                           && $boxes.eq( 6 ).attr( "player" ) === player2;
-
-  if ( player1Combination ) {
-    return "Player 1 win!";
-  }
-  else if ( player2Combination ) {
-    return "Player 2 win!";
-  }
+  $( ".box" ).on( "click", init );
 
 };
 
-// Diagonal checking
-var checkComb8 = function () {
-  var $boxes = $( ".box" );
-
-  var player1Combination = $boxes.eq( 0 ).attr( "player" ) === player1
-                           && $boxes.eq( 4 ).attr( "player" ) === player1
-                           && $boxes.eq( 8 ).attr( "player" ) === player1;
-
-  var player2Combination = $boxes.eq( 0 ).attr( "player" ) === player2
-                           && $boxes.eq( 4 ).attr( "player" ) === player2
-                           && $boxes.eq( 8 ).attr( "player" ) === player2;
-
-  if ( player1Combination ) {
-    return "Player 1 win!";
-  }
-  else if ( player2Combination ) {
-    return "Player 2 win!";
-  }
-
-};
-
- var computerAction = function () {
-   var boxes = $( ".box" );
-   var potentialBoxes = [];
-
-   for ( var i = 0; i < boxes.length; i++ ) {
-     if ( boxes.eq( i ).attr( "isclick" ) === "false" ) {
-       potentialBoxes.push( boxes.eq( i ) );
-     }
-   }
-
-   if ( potentialBoxes.length > 0 ) {
-     var luckyNumber = Math.floor( Math.random() * potentialBoxes.length );
-
-     var $computerBox = $( potentialBoxes[ luckyNumber ] );
-
-    //  var $computerMove = $( computerAction() );
-
-     // Set to notify already clicked
-     $computerBox.attr( "isclick", "true" );
-
-     $computerBox.css( {
-       backgroundImage: "url( images/spongebob.png )",
-       backgroundSize: "cover"
-     } );
-
-     // Set to notify who clicked it
-     $computerBox.attr( "player", player2 );
-
-     currentPlayer = player2;
-
-   }
-
-
-   return null;
-
- }
-
+// ------------- Release the Kraken ---------------
 
 // Set the event listener to the box
 $( ".box" ).on( "click", init );
